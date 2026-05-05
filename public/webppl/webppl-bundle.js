9d3432ca3e077cf3443571f5678d355ac7cebf2c
chimenkamp@gmail.com <chimenkamp@gmail.com>
2026-05-05 06:41:25 +0200

    Rename example files, update config, bump version
    Rename example files to remove spaces/colons (Digital transfer: Discharge.json -> Digital_transfer_Discharge.json, Digital whiteboard: Transfer.json -> Digital_whiteboard_Transfer.json), update example paths in public/examples/example-config.json to match the new filenames, and bump webppl-bundle.js version string from v0.9.15-7e385c7 to v0.9.15-f4e3923.

--- public/webppl/webppl-bundle.js
+++ public/webppl/webppl-bundle.js
@@ -58170,7 +58170,7 @@ var thunkify = require('./syntax').thunkify;
 var cps = require('./transforms/cps').cps;
 var errors = require('./errors/browser');
 var util = require('./util');
-var version = 'v0.9.15-7e385c7';
+var version = 'v0.9.15-f4e3923';
 var packages = [];
 var load = _.once(function () {
     packages.forEach(function (pkg) {
